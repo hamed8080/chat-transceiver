hamed8080/chat-extensions

Merge main into develop

Package.swift
@@ -17,27 +17,27 @@ let package = Package(
             targets: ["ChatExtensions"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.0.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-cache", from: "2.0.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-core", from: "2.0.1"),
+        .package(path: "../ChatTransceiver"),
+        .package(path: "../ChatCache"),
+        .package(path: "../ChatCore"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatExtensions",
             dependencies: [
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ]
         ),
         .testTarget(
             name: "ChatExtensionsTests",
             dependencies: [
                 "ChatExtensions",
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ],
             resources: []
         ),

hamed8080/chat-transceiver

Package.swift
@@ -17,26 +17,26 @@ let package = Package(
             targets: ["ChatTransceiver"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-dto", from: "2.0.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.1"),
+        .package(path: "../ChatDTO"),
+        .package(path: "../Additive"),
+        .package(path: "../Mocks"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatTransceiver",
             dependencies: [
-                .product(name: "ChatDTO", package: "chat-dto"),
-                .product(name: "Additive", package: "additive"),
+                .product(name: "ChatDTO", package: "ChatDTO"),
+                .product(name: "Additive", package: "Additive"),
             ]
         ),
         .testTarget(
             name: "ChatTransceiverTests",
             dependencies: [
                 "ChatTransceiver",
-                .product(name: "ChatDTO", package: "chat-dto"),
-                .product(name: "Additive", package: "additive"),
-                .product(name: "Mocks", package: "mocks"),
+                .product(name: "ChatDTO", package: "ChatDTO"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "Mocks", package: "Mocks"),
             ],
             resources: []
         ),